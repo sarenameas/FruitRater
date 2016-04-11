5f32d2aa01a48c96f1bfed5fe224e035abb9731c
Sarena Meas <SMeas@UASC.com>
2016-04-11 11:52:45 -0700

    Moved Yelp services to own module.

--- app/app.js
+++ app/app.js
@@ -4,4 +4,5 @@ module.exports = function(app, mongoose, db) {
 
     var UserService = require('./services/user.service.server.js')(app, UserModel);
     var ReviewService = require('./services/review.service.server.js')(app, ReviewModel);
+    var YelpService = require('./services/yelp.service.server')(app);
 };
