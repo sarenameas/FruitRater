e9d3b112341b572e72902849e44597c9e2388966
smeas <smeas@ccs.neu.edu>
2016-04-17 19:24:05 -0700

    Downloaded necessary modules for passport and set up passport and session in the server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var session = require('express-session');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
@@ -23,11 +25,22 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 
 // connect to the database
 var db = mongoose.connect(connectionString);
-
 mongoose.Promise = require('q').Promise;
 
+// Parse request body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport and session initialization
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(express.static(__dirname + '/public'));
 
